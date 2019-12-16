7952f5f98f6d751adf1b55b3ec05360243270a30
RomanS <RomanS@ggg.rrr>
2019-12-16 20:49:30 +0200

    Get new information from worc computer - Added new register_model.php file and modifated register.php file

--- register.php
+++ register.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,7 +22,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="index.html">
+                <a class="navbar-brand" href="index.php">
                     Project
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -52,13 +57,13 @@
                             <div class="card-header">Register</div>
 
                             <div class="card-body">
-                                <form method="POST" action="">
+                                <form method="POST" action="store/register_model.php">
 
                                     <div class="form-group row">
                                         <label for="name" class="col-md-4 col-form-label text-md-right">Name</label>
 
                                         <div class="col-md-6">
-                                            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="" autofocus>
+                                            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" autofocus>
 
                                                 <span class="invalid-feedback" role="alert">
                                                     <strong>Ошибка валидации</strong>
@@ -68,9 +73,17 @@
 
                                     <div class="form-group row">
                                         <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
-
                                         <div class="col-md-6">
                                             <input id="email" type="email" class="form-control" name="email" >
+                                            <?php if($_SESSION['email_is_not_validated'])
+                                            { ?>
+                                            <div class="alert alert-warning" role="alert">
+                                                <!-- добавление комментария-->
+                                                <?php echo $_SESSION['email_is_not_validated'];?>
+                                                <?php
+                                                session_destroy();
+                                                }  ?>
+                                            </div>
                                         </div>
                                     </div>
 
